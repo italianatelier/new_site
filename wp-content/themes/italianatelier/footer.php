38c240ee03dc7edfef59fe713601a80d720f9884
Hai Tran <haitranthanh93@gmail.com>
2021-06-05 17:00:30 +0700

    update footer

--- wp-content/themes/italianatelier/footer.php
+++ wp-content/themes/italianatelier/footer.php
@@ -3,8 +3,91 @@
 		</div><!-- #primary -->
 
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
+	<footer id="site-footer" class="" role="contentinfo">
+		<div class="container">
+			<div id="footer-logo" class="text-center">
+				<a href="<?php echo home_url()?>"><img src="<?php echo THEME_URL.'/assets/images/footer-logo.png'?>" alt="" /></a>
+			</div>
+			<div id="footer-cont">
+				<aside id="footer-info">
+
+				</aside>
+				<aside id="footer-branches">
+					<ul class="items row">
+						<li class="col-12 col-sm-4">
+							<div class="item-branch">
+								<h4 class="tit">Milano</h4>
+								<div class="cont">
+									Via Montenapoleone 8<br>
+									20121 Milano <br>
+									Italy
+								</div>
+							</div>
+						</li>
+						<li class="col-12 col-sm-4">
+							<div class="item-branch">
+								<h4 class="tit">Pistoia</h4>
+								<div class="cont">
+									Via Cavour 37<br>
+									51100 Pistoia <br>
+									Italy
+								</div>
+							</div>
+						</li>
+						<li class="col-12 col-sm-4">
+							<div class="item-branch">
+								<h4 class="tit">Mumbai</h4>
+								<div class="cont">
+									Peninsula Business Park<br>
+									Tower B 1107 <br>
+									11th Floor Regus <br>
+									S.B. Road, Lower Parel <br>
+									400013 Mumbai <br>
+									India
+								</div>
+							</div>
+						</li>
+						<li class="col-12 col-sm-4">
+							<div class="item-branch">
+								<h4 class="tit">Singapore</h4>
+								<div class="cont">
+									30 Cecil Street<br>
+									#19-08 Prudential Tower <br>
+									049712 Singapore <br>
+									Singapore
+								</div>
+							</div>
+						</li>
+						<li class="col-12 col-sm-4">
+							<div class="item-branch">
+								<h4 class="tit">Shanghai</h4>
+								<div class="cont">
+									Rm 208, 1003 Wuding Road<br>
+									Jing’an Dist <br>
+									200042 Shanghai <br>
+									China
+								</div>
+							</div>
+						</li>
+						<li class="col-12 col-sm-4">
+							<div class="item-branch">
+								<h4 class="tit">Ho Chi Minh</h4>
+								<div class="cont">
+									Floor 10th, Pax Sky Building<br>
+									51 Nguyen Cu Trinh <br>
+									Ward Nguyen Cu Trinh, D.1 <br>
+									700000 Ho Chi Minh <br>
+									Vietnam
+								</div>
+							</div>
+						</li>
+					</ul>
+				</aside>
+			</div>
+		</div>
 		
+
+		</div>
 	</footer><!-- #colophon -->
 
 </div><!-- #page -->
